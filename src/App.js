f58fcf9bf119fa4c7787a7dc1b49dbc6c8273fa6
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-10 23:16:14 +0300

    added-expenseList-component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import ExpensesList from "./components/ExpensesList";
 
 function App() {
     const expenses = [
@@ -11,12 +12,8 @@ function App() {
     ];
   return (
     <div>
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
-        <ExpenseItem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].date}/>
-        <ExpenseItem title={expenses[5].title} amount={expenses[5].amount} date={expenses[5].date}/>
+      <ExpensesList items={expenses} />
+    
     </div>
   );
 }
---
+++ src/components/ExpensesList.jsx
@@ -0,0 +1,21 @@
+import './ExpensesList.css';
+import ExpenseItem from './ExpenseItem';
+import { Component } from 'react';
+
+export default function ExpensesList({items}) {
+	console.log(ExpenseItem);
+	return (
+		<div className="expenses">
+			<ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date}/>
+			<ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date}/> 
+			<ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date}/>
+			<ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date}/>
+			<ExpenseItem title={items[4].title} amount={items[4].amount} date={items[4].date}/>
+			<ExpenseItem title={items[5].title} amount={items[5].amount} date={items[5].date}/> 
+		</div>
+	)
+}
+
+// {[items]} => {[]} => destructuring => recieves directly array from the main App.js directory which we passed in our ExpenseList Component
+// as an attribute and we catch it here with props : (items). Then we pass title,amount and date attributes into the expenseItem component
+// which he resolves them by himself. Passing around data is the point of using props. Then in the date we pass the date props to the expenseDate component
