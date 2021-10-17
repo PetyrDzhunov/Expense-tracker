69695733f276adc83ac04306703c340c220b5730
PetyrDzhunov <petar.djunov@abv.bg>
2021-10-17 16:08:16 +0300

    added-expense-form&working-with-multiple-states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpensesList from "./components/Expenses/ExpensesList";
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
     const expenses = [
@@ -11,8 +12,8 @@ function App() {
     ];
   return (
     <div>
+    <NewExpense />
       <ExpensesList items={expenses} />
-    
     </div>
   );
 }
