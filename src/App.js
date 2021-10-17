31f623e7c7425f48db57ad651b0d1d24a186d485
PetyrDzhunov <petar.djunov@abv.bg>
2021-10-17 17:38:24 +0300

    child-parent-component-communication-pattern

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
     const expenses = [
-        {title:"Car Insurance" , amount: 294.67,date: new Date(2021,2,19)},
-        {title:"Toilet Paper" , amount: 20.97,date : new Date(2021,3,22)},
-        {title:"New Desk" , amount: 354.27,date : new Date(2021,4,14)},
-        {title:"New Car" , amount: 3850,date : new Date(2021,5,3)},
-        {title:"New Laptop" , amount: 1250,date : new Date(2021,6,8)},
-        {title:"Food expense" , amount: 200,date : new Date(2021,7,30)},
+        {id:'e1',title:"Car Insurance" , amount: 294.67,date: new Date(2021,2,19)},
+        {id:'e2',id:'e1',id:'e1',title:"Toilet Paper" , amount: 20.97,date : new Date(2021,3,22)},
+        {id:'e3',id:'e1',title:"New Desk" , amount: 354.27,date : new Date(2021,4,14)},
+        {id:'e4',title:"New Car" , amount: 3850,date : new Date(2021,5,3)},
     ];
+    const addExpenseHandler = (expense) => {
+    console.log(expense);
+    }
   return (
     <div>
-    <NewExpense />
+    <NewExpense onAddExpense={addExpenseHandler} />
       <ExpensesList items={expenses} />
     </div>
   );
